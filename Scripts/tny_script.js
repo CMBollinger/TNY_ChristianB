6b377d7f3a30d498c8799cb8136533accda0a196
CMBollinger1s <CMBollinger1s@capetigers.com>
2024-09-30 10:31:54 -0500

    First Commit

---
+++ Scripts/tny_script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Christian Bollinger
+   Date:   9/30/24
+
+*/
+
